80db6cef3df4336997f1876715a026b899287178
kuujhin <kdoubleu0711@gmail.com>
2024-01-19 02:43:30 +0900

    fix: bookmark duplicate

--- src/controller/bookmark/controller.ts
+++ src/controller/bookmark/controller.ts
@@ -61,8 +61,17 @@ export const getBookmarkList: RequestHandler = async (req, res, next) => {
 export const saveBookmark: RequestHandler = async (req, res, next) => {
   try {
     const user = req.session.user;
+    const userId = user?.id;
     const placeId = Number(req.body.placeId);
     // console.log(`bookmark controller saveBookmark ${placeId}`);
+    if (!userId) throw new BadRequestError('히스토리 저장 실패');
+    const bookmark: GetBookmark = await BookmarkService.getBookmarkByPlaceId(
+      userId,
+      placeId,
+    );
+    if(bookmark) {
+      res.status(202).json(bookmark.id);
+    }
     const place = await PlaceService.getPlaceById(placeId);
     console.log(`bookmark controller saveBookmark ${place}`);
     if (!user || !place) throw new BadRequestError('히스토리 저장 실패');
